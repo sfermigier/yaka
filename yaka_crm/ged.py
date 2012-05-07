34d461228b5d7735e5e164763b9c65f6659d7374
Stefane Fermigier <sf@fermigier.com>
2012-05-07 11:05:16 +0200

    Prepare for multi-apps.

--- yaka_crm/ged.py
+++ yaka_crm/ged.py
@@ -4,8 +4,36 @@ Don't worry, it's just a prototype. Will be refactored later.
 """
 
 from flask import Blueprint, render_template
+from sqlalchemy.types import UnicodeText, LargeBinary, Integer
+
+from yaka_crm.core.entities import Entity, Column
+
 
 ged = Blueprint("ged", __name__, url_prefix="/ged")
+
+#
+# Entities and forms
+#
+class Folder(Entity):
+  __tablename__ = 'folder'
+
+  name = Column(UnicodeText)
+  parent_id = Column(Integer)
+
+
+class File(Entity):
+  __tablename__ = 'file'
+
+  name = Column(UnicodeText)
+  parent_id = Column(Integer)
+
+  text = Column(UnicodeText)
+  data = Column(LargeBinary)
+
+
+#
+# Controllers
+#
 
 @ged.route("/")
 def home():
