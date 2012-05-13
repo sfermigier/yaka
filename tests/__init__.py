0fa8d3b766f6061f5b37376bb94c970ff37e27c2
Stefane Fermigier <sf@fermigier.com>
2012-05-13 20:42:08 +0200

    Fix conversion unit test bug.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,9 @@
 # Hack around the fact that twill (a requirement for Flask-Testing) embeds
 # an older version of subprocess.
+
+import os
+
+print os.environ['PATH']
+os.environ['PATH'] = "/usr/local/bin:" + os.environ['PATH']
+
 import subprocess
--- yaka_crm/services/conversion.py
+++ yaka_crm/services/conversion.py
@@ -4,6 +4,7 @@ import subprocess
 
 
 # TODO: make asynchronous
+# TODO: deal with other content types
 def convert(f):
   tmp_in_fn = tempfile.mktemp()
   tmp_in_fd = open(tmp_in_fn, 'wc')
@@ -13,17 +14,13 @@ def convert(f):
   tmp_out_fn = tempfile.mktemp()
 
   if f.mime_type == 'application/pdf':
-    print subprocess.__file__
-    print 'pdftotext', tmp_in_fn, tmp_out_fn
-    subprocess.check_output(['/usr/local/bin/pdftotext', tmp_in_fn, tmp_out_fn])
+    subprocess.check_call(['pdftotext', tmp_in_fn, tmp_out_fn])
     text = open(tmp_out_fn).read()
     f.text = unicode(text, 'utf8', errors='ignore')
     os.unlink(tmp_out_fn)
 
-    subprocess.check_output(['pdftoppm', '-singlefile', '-jpeg', '-l', '1', tmp_in_fn, tmp_out_fn])
+    subprocess.check_call(['pdftoppm', '-singlefile', '-jpeg', '-l', '1', tmp_in_fn, tmp_out_fn])
     preview = open(tmp_out_fn + '.jpg').read()
     f.preview = preview
-    print tmp_out_fn + '.jpg'
-    #os.unlink(tmp_out_fn)
 
   os.unlink(tmp_in_fn)
