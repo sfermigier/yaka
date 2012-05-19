3a2b4d00a07d2aeec97887a7d03abde0bb572e8b
Stefane Fermigier <sf@fermigier.com>
2012-05-19 19:38:22 +0200

    Deployment.

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,21 @@
-from fabric.api import env, run, cd, local, put, sudo
+from fabric.api import env, run, cd, local, put, sudo, task
 
-env.hosts = ['oss4cloud.org']
+APT_PREFIX = "DEBCONF_TERSE=yes DEBIAN_PRIORITY=critical DEBIAN_FRONTEND=noninteractive"
+
 
 STAGING_DIR = "yaka-crm/staging"
 PRODUCTION_DIR = "yaka-crm/production"
 NATIVE_PACKAGES = "python-pip poppler-utils imagemagick python-dev " \
-    "libjpeg-dev libxslt1-dev make libreoffice unoconv"
+    "libjpeg-dev libxslt1-dev make libreoffice unoconv libzmq-dev"
 
 
+@task
+def prod():
+  env.hosts = ['oss4cloud.org']
+  env.user = 'yaka'
+
+
+@task
 def vagrant():
   # change from the default user to 'vagrant'
   env.user = 'vagrant'
@@ -19,44 +27,62 @@ def vagrant():
   env.key_filename = result.split()[1]
 
 
-def update():
-  """Updates Debian/Ubuntu's package list"""
-  sudo('apt-get -y update')
-
+@task
 def upgrade():
   """Perform a safe upgrade"""
-  update()
-  sudo('aptitude -yv safe-upgrade')
+  sudo('apt-get -y update')
+  sudo('%s aptitude -y safe-upgrade' % APT_PREFIX)
 
+
+@task
 def setup():
-  # XXX: Is this really needed?
-  ENV = "DEBCONF_TERSE=yes DEBIAN_PRIORITY=critical DEBIAN_FRONTEND=noninteractive"
-  sudo("export %s" % ENV)
-
   for pkg in NATIVE_PACKAGES.split(" "):
-    sudo("%s apt-get -yu install %s" % (ENV, pkg))
+    sudo("%s apt-get -yu install %s" % (APT_PREFIX, pkg))
+
   sudo("pip -q install virtualenv")
   sudo("pip -q install tox")
 
+  run("mkdir -p %s" % STAGING_DIR)
+  run("mkdir -p %s" % PRODUCTION_DIR)
+  with cd(PRODUCTION_DIR):
+    run("virtualenv env")
+
+
+@task
 def push():
   local("make clean")
   local("rm -f /tmp/yaka.tgz")
   local("tar czf /tmp/yaka.tgz --exclude .git --exclude .tox .")
   put("/tmp/yaka.tgz", "/tmp/yaka.tgz")
 
-def stage():
-  push()
 
-  run("mkdir -p %s" % STAGING_DIR)
+@task
+def stage():
   with cd(STAGING_DIR):
     run("tar zxf /tmp/yaka.tgz")
     run("tox -e py27")
 
-def deploy():
-  stage()
 
-  run("mkdir -p %s" % PRODUCTION_DIR)
+@task
+def deploy():
   with cd(PRODUCTION_DIR):
     run("tar zxf /tmp/yaka.tgz")
-    run("virtualenv env")
-    run("env/bin/python setup.py develop -U")
+    run("env/bin/python setup.py develop")
+    run("env/bin/pip install circus")
+
+    circus_ini = open("circus.ini").read()
+    circus_ini = circus_ini.replace("@@HOME@@", "/users/" + env.user)
+    with open("/tmp/circus.ini") as fd:
+      fd.write(circus_ini)
+    put("/tmp/circus.ini", "circus.ini")
+
+    run("if [ ! -e circus.pid ] ; then env/bin/circusd --daemon --pidfile circus.pid circus.ini; fi;")
+    run("env/bin/circusctl reload")
+
+
+@task(default=True)
+def default():
+  push()
+  stage()
+  deploy()
+
